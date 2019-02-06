d44846e8d619272e3b405379f35f4432afc139ca
Matthew Brucker <mpbrucker@gmail.com>
2019-02-05 23:29:01 -0500

    Adding mouse drag controls.

--- assets/js/svg.js
+++ assets/js/svg.js
@@ -1,35 +1,42 @@
-// var path = new Path();
-// // Give the stroke a color
-// path.strokeColor = 'black';
-// var start = new Point(100, 100);
-// // Move to start and draw a line from there
-// path.moveTo(start);
-// // Note the plus operator on Point objects.
-// // PaperScript does that for us, and much more!
-// path.lineTo(start + [ 100, -50 ]);
 
 var ghLogo = project.importSVG(document.getElementById('gh-logo'), {expandShapes: true, insert: true});
-// project.importSVG('/assets/ghlogo.svg', {expandShapes: true, insert: true});
-// ghLogo.place();
-console.log(ghLogo.children);
-var i;
-
 var gPath;
 var hPath;
+var prevMouse = new Point(0,0);
+var isMouseDown = false;
+
+
+
+var i;
 for (i = 0; i < ghLogo.children.length; i++) {
-    // const pathName = ghLogo.children[i].name;
-    // if (pathName == 'g-path') {
-    //     gPath = ghLogo.children[i];
-    // } else if (pathName == 'h-path') {
-    //     hPath = ghLogo.children[i];
-    // }
+    if (ghLogo.children[i].name == 'g-path') {
+        gPath = ghLogo.children[i];
+    } else if (ghLogo.children[i].name == 'h-path') {
+        hPath = ghLogo.children[i];
+    }
     // const child = ghLogo.children[i];
     // console.log(child.id)
     console.log(ghLogo.children[i].name)
 }
 // console.log(project.activeLayer.children);
 
-// hPath.onMouseDown = function(e) {
-//     console.log('click');
-// }
+hPath.onMouseDown = function(e) {
+    isMouseDown = true;
+    prevPoint = e.point;
+}
+
+project.view.onMouseUp = function(e) {
+    isMouseDown = false;
+    prevPoint = e.point;
+}
+
+project.view.onMouseMove = function(e) {
+    if (isMouseDown) {
+        var pointDiff = Object.assign({}, e.point-prevPoint)
+        var yDiff = (e.point-prevPoint).x/Math.sqrt(3)
+        hPath.translate(new Point(pointDiff.x, yDiff));
+        console.log(e.point-prevPoint);
+    }
+    prevPoint = e.point;
+}
 
